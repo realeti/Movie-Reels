d368ddf2c9fa6e1dc619c7d5c8275f982f321329
bristol <oro.eto.bristol@gmail.com>
2024-01-30 18:16:28 +0200

    Initial Commit

---
+++ Nitrix-Movie-Reels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nitrix-Movie-Reels
+//
+//  Created by Apple M1 on 30.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
